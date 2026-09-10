1b0f00c071ffa78f4701070016f76bf95c4e47cf
bryndisgunnlaugsd <bryndisg24@ru.is>
2026-09-10 13:29:54 +0000

    cheese og pepperoni pizza

--- code_skeleton 2/factory_skeleton/pizzas/CheesePizza.py
+++ code_skeleton 2/factory_skeleton/pizzas/CheesePizza.py
@@ -1,6 +1,18 @@
+from ingredient_factories.PizzaIngredientFactory import PizzaIngredientFactory
+from pizzas.Pizza import Pizza
 
+# CheesePizza is a product (PizzaStore creates it)
+# also a client (it consumes a PizzaIngredientFactory to get its ingredients)
 
-class CheesePizza:
-    pass
+class CheesePizza(Pizza):
 
+    def __init__(self, ingredient_factory: PizzaIngredientFactory):
+        self._ingredient_factory = ingredient_factory
 
+    def prepare(self) -> None:
+        print(f'preparing: {self.get_name()}')
+        # Which factory we are given determines what comes back here
+        # ThinCrustDough vs. ThickCrustDough
+        self._dough = self._ingredient_factory.create_dough()
+        self.cheese = self._ingredient_factory.create_cheese()
+        self.sauce = self._ingredient_factory.create_sauce()
--- code_skeleton 2/factory_skeleton/pizzas/PepperoniPizza.py
+++ code_skeleton 2/factory_skeleton/pizzas/PepperoniPizza.py
@@ -1,6 +1,16 @@
 from ingredient_factories.PizzaIngredientFactory import PizzaIngredientFactory
 from pizzas.Pizza import Pizza
 
+# Same thing as CheesePizza
 
-class PepperoniPizza:
-    pass
+class PepperoniPizza(Pizza):
+
+    def __init__(self, ingredient_factory: PizzaIngredientFactory):
+        self._ingredient_factory = ingredient_factory
+
+    def prepare(self) -> None:
+        print(f'preparing: {self.get_name}')
+        self._dough = self._ingredient_factory.create_dough()
+        self._cheese = self._ingredient_factory.create_cheese()
+        self._sauce = self._ingredient_factory.create_sauce()
+        self._veggies = self._ingredient_factory.create_veggies()
