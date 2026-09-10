2f247b7f95a309e47c5f2adefbed2d55b2bd1446
bryndisgunnlaugsd <bryndisg24@ru.is>
2026-09-10 13:43:36 +0000

    pizza store virkni

--- code_skeleton 2/factory_skeleton/PizzaStores/PizzaStore.py
+++ code_skeleton 2/factory_skeleton/PizzaStores/PizzaStore.py
@@ -1,5 +1,18 @@
+from abc import ABC, abstractmethod
 
+from pizzas.Pizza import Pizza
+from pizzas.PizzaType import PizzaType
 
-class PizzaStore:
-    pass
+class PizzaStore(ABC):
 
+    def order_pizza(self, pizza_type: PizzaType) -> Pizza:
+        pizza = self.create_pizza(pizza_type)
+        pizza.prepare()
+        pizza.bake()
+        pizza.cut()
+        pizza.box()
+        return pizza
+
+    @abstractmethod
+    def create_pizza(self, pizza_type: PizzaType) -> Pizza:
+        pass
